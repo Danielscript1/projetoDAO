3cfb900239ebe3172be59eb3bad2fb99188d1ac3
Danielscript1 <danielsousagit@gmail.com>
2021-11-04 19:54:17 -0300

    classe

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,75 @@
+package model.entities;
+
+import java.util.Date;
+
+public class Seller {
+	//atributos
+	private Integer id;
+	private String name;
+	private String email;
+	private Date bithDate;
+	private Double baseSalary;
+	
+	//ligacão Department -> seller (um para um)
+	private Department department;
+
+	public Seller(Integer id, String name, String email, Date bithDate, Double baseSalary, Department department) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.bithDate = bithDate;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBithDate() {
+		return bithDate;
+	}
+
+	public void setBithDate(Date bithDate) {
+		this.bithDate = bithDate;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+	
+	
+}

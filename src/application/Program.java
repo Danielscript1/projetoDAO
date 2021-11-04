2950038feaa67e778c760700fb6bb9803f305734
Danielscript1 <danielsousagit@gmail.com>
2021-11-04 20:14:17 -0300

    classe2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,11 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
-import db.DB;
-import db.DbException;
-import db.DbIntegrityException;
 
 public class Program {
 
 	public static void main(String[] args)  {
+		
 		
 	}
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,8 +1,10 @@
 package model.entities;
 
+import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
-public class Seller {
+public class Seller implements Serializable{
 	//atributos
 	private Integer id;
 	private String name;
@@ -68,6 +70,29 @@ public class Seller {
 
 	public void setDepartment(Department department) {
 		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", bithDate=" + bithDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
 	}
 	
 	
